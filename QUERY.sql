50318f2cf009c1702b78ac42e20e6c6cf4b07932
jany-coder <jany.java@gmail.com>
2026-06-22 10:39:10 +0600

    create matches table

--- QUERY.sql
+++ QUERY.sql
@@ -24,3 +24,14 @@ CREATE TABLE Users (
     phone_number VARCHAR(20)
 );
 
+-- =========================================================================
+-- 2. CREATE MATCHES TABLE
+-- =========================================================================
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price NUMERIC(10,2) NOT NULL CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(20) NOT NULL CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
+
